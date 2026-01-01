47a698cbcce117254154df570a41eaaa27a69ee3
moonseungju <msj9965@gmail.com>
2026-01-01 23:28:17 +0900

    Refactor Category and Order entities to improve relationship management and cascade operations

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -31,6 +31,12 @@ public class Category {
     private Category parent;
 
     @OneToMany(mappedBy = "parent")
-    private List<Category> children = new ArrayList<>();
+    private List<Category> child = new ArrayList<>();
+
+    //==연관관계 메서드==//
+    public void addChildCategory(Category child){
+        this.child.add(child);
+        child.setParent(this);
+    }
 
 }
